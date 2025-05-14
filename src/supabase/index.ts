c3b1c0aad5e3a417ec929a0a1afb0c4caab53fe3
Rahul <imrahul2308@gmail.com>
2025-05-15 04:37:16 +0530

    Update index.ts

--- src/supabase/index.ts
+++ src/supabase/index.ts
@@ -1,3 +1,2 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://ofihmoordccqsdybmcva.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9maWhtb29yZGNjcXNkeWJtY3ZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMzY2MDYsImV4cCI6MjA2MjgxMjYwNn0.vQLBP3wIbjIrD3_TmCQdqf6W8MkNVL-2Ouw_7g579k8')
